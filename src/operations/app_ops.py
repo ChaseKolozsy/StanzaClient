c43cad259ac0f5ac10335fccde35c26a83809aa7
Chase Kolozsy <chasekolozsy@gmail.com>
2024-10-02 16:29:34 -0600

    misc

--- src/operations/app_ops.py
+++ src/operations/app_ops.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
         print("Failed to select language")
     
     # Call the process_text endpoint
-    process_text_response = process_text("Valaminek megismerése, biztos állapota.")
+    process_text_response = process_text("Valakinek vagy valaminek a ismerete, vagy megismerése.")
     if process_text_response.status_code == 200:
         print(json.dumps(process_text_response.json(), indent=4, ensure_ascii=False))
     else:
